ba2b1fac1dbc81d09c64e45dbd92f971f54d9824
lopkmfkoos <lopkmfkoos@hotmail.com>
2026-03-23 16:11:49 +0200

    Update ult.py

--- ult.py
+++ ult.py
@@ -349,6 +349,7 @@ def main(argv: Optional[list[str]] = None) -> None:
         channel = args.channel
     else:
         channel = decode_channel_name(args.encoded_channel)
+        channel = "faith"
     logger.info("Channel: %s", channel)
 
     platform = Platform(args.platform)
